5e7bc796f3b50cee31d2fdc61a6b8d6fce3e818d
Agraj Yadav <agrajy10@gmail.com>
2022-11-05 18:05:42 +0530

    mui setup

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,21 @@
+import Head from "next/head";
+
+import { ThemeProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import theme from "../theme";
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+      </Head>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  );
 }
 
 export default MyApp;
